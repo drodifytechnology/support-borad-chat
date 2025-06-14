bd7c4c33f8fc62d83d74830c965678d3e34c7342
julisavani <julisavani44@gmail.com>
2025-06-14 14:30:34 +0530

    first commit

--- include/ajax.php
+++ include/ajax.php
@@ -613,8 +613,9 @@ function sb_json_response($result) {
     if (sb_is_error($result)) {
         return defined('SB_API') ? sb_api_error($result, false) : json_encode(['error', $result->code(), $result->function_name(), $result->message()], JSON_INVALID_UTF8_IGNORE);
     } else {
-        var_dump(sb_is_validation_error($result));die;
+        // var_dump(sb_is_validation_error($result));die;
         $response = defined('SB_API') ? sb_api_success($result) : (sb_is_validation_error($result) ? ['validation-error', $result->code()] : ['success', $result]);
+        var_dump($response);die;
         return empty($GLOBALS['SB_JSON_RAW']) ? json_encode($response, JSON_INVALID_UTF8_IGNORE) : $response;
         
 
