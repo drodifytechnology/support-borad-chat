8e75f9c17657cb8924529a30b750607a3d029891
julisavani <julisavani44@gmail.com>
2025-06-14 14:15:37 +0530

    first commit

--- include/ajax.php
+++ include/ajax.php
@@ -609,7 +609,7 @@ function sb_ajax_execute() {
 
 function sb_json_response($result) {
     
-    print_r($result);die;
+    echo is_a($result, 'SBError');die;
     if (sb_is_error($result)) {
         return defined('SB_API') ? sb_api_error($result, false) : json_encode(['error', $result->code(), $result->function_name(), $result->message()], JSON_INVALID_UTF8_IGNORE);
     } else {
