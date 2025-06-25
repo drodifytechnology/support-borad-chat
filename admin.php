9065a30f6904873ee381e6f34ce51bd2bb774573
julisavani <julisavani44@gmail.com>
2025-06-25 12:37:40 +0530

    first commit

--- admin.php
+++ admin.php
@@ -10,6 +10,9 @@
  */
 
 header('X-Frame-Options: DENY');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 global $SB_CONNECTION;
 $connection_success = false;
